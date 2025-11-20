fb2e659cc436e77739783b9157559aba24244147
Stepan Skvortsov <126590212+uhrurh4ever@users.noreply.github.com>
2025-11-20 22:55:09 +0300

    Update SecurityConfiguration.java

--- src/main/java/com/example/security/SecurityConfiguration.java
+++ src/main/java/com/example/security/SecurityConfiguration.java
@@ -11,43 +11,25 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-    /**
-     * Бин для создания объекта BCryptPasswordEncoder, который используется для хэширования паролей
-     *
-     * @return Объект BCryptPasswordEncoder
-     */
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    /**
-     * Бин для создания цепочки фильтров безопасности Spring Security (SecurityFilterChain)
-     *
-     * @param http HttpSecurity объект для конфигурирования безопасности
-     * @return SecurityFilterChain объект, представляющий сконфигурированную цепочку фильтров
-     * @throws Exception Ошибки при конфигурировании безопасности
-     */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        // Разрешить доступ к странице регистрации для всех пользователей
                         .requestMatchers("/registration").permitAll()
-                        // Требовать авторизацию для всех остальных запросов
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
-                        // Установить страницу входа по адресу "/login" и разрешить доступ к ней всем пользователям
                         .loginPage("/login")
                         .permitAll()
                 )
                 .logout((logout) -> logout
-                        // Установить URL выхода из системы на "/logout"
                         .logoutUrl("/logout")
-                        // Перенаправлять на главную страницу после успешного выхода
                         .logoutSuccessUrl("/")
-                        // Инвалидировать HttpSession после выхода
                         .invalidateHttpSession(true));
 
         return http.build();
